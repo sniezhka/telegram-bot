e21d4300cafe0597a9e9fc0e77c53c266125bd11
sniezhka <snizhanatalapchuk@gmail.com>
2020-12-19 18:28:14 +0200

    Add bot model

---
+++ TelegramBot/Models/Bot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace TelegramBot.Models
+{
+    class Bot
+    {
+        private readonly ITelegramBotClient _bot;
+
+        public Bot(string accessToken)
+        {
+            this._bot = new TelegramBotClient(accessToken);
+        }
+
+        public void StartReceiving()
+        {
+            _bot.StartReceiving();
+        }
+
+        public void StopReceiving()
+        {
+            _bot.StopReceiving();
+        }
+
+        public void AttachSubscriber(EventHandler<MessageEventArgs> subscriber)
+        {
+            _bot.OnMessage += subscriber;
+        }
+    }
+}
