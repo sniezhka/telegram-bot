3e765725e8b9927416b658f908d10becbffb0326
sniezhka <snizhanatalapchuk@gmail.com>
2020-12-19 18:29:23 +0200

    Add args parser

---
+++ TelegramBot/Args.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PowerArgs;
+
+namespace TelegramBot
+{
+    class Args
+    {       
+        [ArgRequired(PromptIfMissing = true)]
+        public string Token { get; set; }
+    }
+}
