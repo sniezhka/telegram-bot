5164f7bca07e34fc7d54b9bf94e42afef88a4ca0
sniezhka <snizhanatalapchuk@gmail.com>
2020-12-19 18:24:52 +0200

    Init solution

---
+++ Readme.md
@@ -0,0 +1,8 @@
+﻿Run by command:
+
+dotnet run --project TelegramBot -token TELEGRAM_BOT_TOKEN
+
+
+
+2020
+Author: Snizhana Talapchuk
---
+++ TelegramBot/Program.cs
@@ -0,0 +1,33 @@
+﻿using PowerArgs;
+using System;
+using TelegramBot.Models;
+using TelegramBot.Models.Subscribers;
+
+namespace TelegramBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {          
+            try
+            {
+                var parsed = PowerArgs.Args.Parse<Args>(args);
+                (new Program()).bootstrap(parsed.Token);
+            }
+            catch (ArgException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ArgUsage.GenerateUsageFromTemplate<Args>());
+            }
+        }
+
+        public void bootstrap(string token)
+        {
+            Bot bot = new Bot(token);
+
+            bot.StartReceiving();
+
+            Console.ReadKey();
+        }
+    }
+}
